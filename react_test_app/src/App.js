ed38cc29aae79bec78712efe0c71307a0e17dce7
JackCokebb <reo2726@gmail.com>
2022-02-22 22:32:47 +0900

    jsx

--- react_test_app/src/App.js
+++ react_test_app/src/App.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom';
 
 function App(){
     const root = document.getElementById("root");
-    const span = React.createElement("span",{id:"mySpan"}, "Hello! i am span!");
-    ReactDOM.render(span, root);
+    const Title = <h3 id = "title" onMouseEnter={()=> console.log("entered")}>
+        Hello! i am title!
+        </h3>
+    const btn = <button 
+        style={{
+            backgroundColor : "tomato",
+        }} 
+        onClick={
+            () => console.log("clicked")
+        }>click me!</button>
+//     const h3 = React.createElement("h3",{id:"mySpan", 
+//     onMouseEnter: ()=> console.log("entered"),
+// }, "Hello! i am span!");
+//     const btn = React.createElement("button",{
+//         onClick: () => console.log("clicked"),
+//     }, "Click me!");
+    const container = React.createElement("div",null, [Title, btn]);
+    ReactDOM.render(container, root);
 }
 
 export default App;
