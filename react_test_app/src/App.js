a8f93c690901e26c8064ce458af0001c068af5a2
JackCokebb <reo2726@gmail.com>
2022-02-22 23:11:26 +0900

    jsx & rendering

--- react_test_app/src/App.js
+++ react_test_app/src/App.js
@@ -3,26 +3,25 @@ import ReactDOM from 'react-dom';
 
 function App(){
     const root = document.getElementById("root");
-    const Title = <h3 id = "title" onMouseEnter={()=> console.log("entered")}>
-        Hello! i am title!
-        </h3>
-    const btn = <button 
-        style={{
-            backgroundColor : "tomato",
-        }} 
-        onClick={
-            () => console.log("clicked")
-        }>click me!</button>
-//     const h3 = React.createElement("h3",{id:"mySpan", 
-//     onMouseEnter: ()=> console.log("entered"),
-// }, "Hello! i am span!");
-//     const btn = React.createElement("button",{
-//         onClick: () => console.log("clicked"),
-//     }, "Click me!");
-    const container = React.createElement("div",null, [Title, btn]);
-    ReactDOM.render(container, root);
+    let counter = 0;
+    
+    //not a good way to re-rendering
+    function countUp(){
+        counter +=1;
+        Render();
+    }
+    function Render(){
+        ReactDOM.render(<Container/>, root);
+    }
+    const Container =()=> (
+    <div>
+        <h3>total clicks: {counter}</h3>
+        <button onClick={countUp}>click me!</button>
+    </div>);
+
+    Render();
 }
 
 export default App;
 
-//start from #2.3
+//start from #3.1
