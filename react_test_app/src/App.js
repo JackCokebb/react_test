e198f9a83b56f92e40ca09951412e3ad5e7965eb
JackCokebb <reo2726@gmail.com>
2022-07-02 22:53:44 +0900

    22.07.02 cleanup func

--- react_test_app/src/App.js
+++ react_test_app/src/App.js
@@ -1,49 +1,33 @@
-//import Button from "./Button";
-//import styles from "./App.module.css"
-
-// useEffect allow us to render some code only once at the first time
-// so that even if some states are changed, some code will not be re-rendered.
-// useEffect function renders the things after all the other things are rendered
 import { useState, useEffect } from "react";
 
+//another component
+//component is the function that return jsx
+// uppercase needed
+function Hello(){
+  //log will appear only when component is created at first time.
+  useEffect(()=>{
+    console.log("created!");
+
+    // the function that will be run when the component is destroyed 
+    // it is return func of useEffect func
+    // this is called **cleanup function**
+    return ()=> console.log("destroyed :(")
+  },[])
+  return <h1>Hello!</h1>;
+}
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue(prev => prev + 1);
-  const onChange = (event) => {
-    setKeyword(event.target.value)
-  }
-  console.log("i run this all the time");
-  // const iRunThisOnce = ()=> {
-  //   console.log("i run only once");
-  // }
-
-  // useEffect allow us to render some code only once at the first time
-  // as [] is empty, react watching nothing --> so react will run this code only one time.
-  //useEffect(iRunThisOnce,[])
-  useEffect(()=>{
-    console.log("i run only once")
-  },[]);
-
-  // when i want to run this code only when this keyword changes
-  // (not when counter changes)
-  // As i write [keyword] --> telling react to run this code if this keyword changes(=watch the keyword)
-  useEffect(()=>{
-    console.log("i run when 'keyword' changes", {keyword});
-  },[keyword]);
-  useEffect(()=>{
-    console.log("i run when 'counter' changes");
-  }, [counter])
-  useEffect(()=>{
-    console.log("i run when 'keyword' and 'counter' changes");
-  }, [keyword, counter])
   
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing(prev => !prev);
+  }
+
+  // we are creating and destroying component with ' showing ? "Hide" : "Show" '
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="search here..."/>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me!!</button>
+      <button onClick={onClick}>{showing ? "Hide" : "Show" }</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
