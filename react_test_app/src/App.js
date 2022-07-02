dc31c7bca6e27da777a6f66e91adc00fce626ae1
JackCokebb <reo2726@gmail.com>
2022-07-02 22:27:42 +0900

    22.07.02 useEffect

--- react_test_app/src/App.js
+++ react_test_app/src/App.js
@@ -1,14 +1,49 @@
-import Button from "./Button";
-// create-react-app change CSS code into js object
-// and this js object have btn inside.
-import styles from "./App.module.css"
+//import Button from "./Button";
+//import styles from "./App.module.css"
+
+// useEffect allow us to render some code only once at the first time
+// so that even if some states are changed, some code will not be re-rendered.
+// useEffect function renders the things after all the other things are rendered
+import { useState, useEffect } from "react";
 
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue(prev => prev + 1);
+  const onChange = (event) => {
+    setKeyword(event.target.value)
+  }
+  console.log("i run this all the time");
+  // const iRunThisOnce = ()=> {
+  //   console.log("i run only once");
+  // }
+
+  // useEffect allow us to render some code only once at the first time
+  // as [] is empty, react watching nothing --> so react will run this code only one time.
+  //useEffect(iRunThisOnce,[])
+  useEffect(()=>{
+    console.log("i run only once")
+  },[]);
+
+  // when i want to run this code only when this keyword changes
+  // (not when counter changes)
+  // As i write [keyword] --> telling react to run this code if this keyword changes(=watch the keyword)
+  useEffect(()=>{
+    console.log("i run when 'keyword' changes", {keyword});
+  },[keyword]);
+  useEffect(()=>{
+    console.log("i run when 'counter' changes");
+  }, [counter])
+  useEffect(()=>{
+    console.log("i run when 'keyword' and 'counter' changes");
+  }, [keyword, counter])
+  
   return (
     <div>
-      <h1 className={styles.title}>Welcome back to React</h1>
-      <Button text={"setsetset"} />
+      <input value={keyword} onChange={onChange} type="text" placeholder="search here..."/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me!!</button>
     </div>
   );
 }
